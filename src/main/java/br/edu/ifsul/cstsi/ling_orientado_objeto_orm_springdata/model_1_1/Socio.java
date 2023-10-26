fefa465dc482c6c696de1311075915fe7f9e96cb
LuigiLoeck <luigisloeck@gmail.com>
2023-10-26 11:33:04 -0300

    models 1_1 1_2

---
+++ src/main/java/br/edu/ifsul/cstsi/ling_orientado_objeto_orm_springdata/model_1_1/Socio.java
@@ -0,0 +1,18 @@
+package br.edu.ifsul.cstsi.ling_orientado_objeto_orm_springdata.model_1_1;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Socio {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private LocalDate aniversario;
+}
