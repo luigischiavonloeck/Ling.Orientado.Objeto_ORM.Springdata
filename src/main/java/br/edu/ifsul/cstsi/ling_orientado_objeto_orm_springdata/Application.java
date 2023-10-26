aea80c2a9a3f232794a79ce6a5f163cfdc05e8e1
LuigiLoeck <luigisloeck@gmail.com>
2023-10-26 11:13:23 -0300

    project configuration

---
+++ src/main/java/br/edu/ifsul/cstsi/ling_orientado_objeto_orm_springdata/Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.ling_orientado_objeto_orm_springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsul/cstsi/ling_orientado_objeto_orm_springdata/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.ling_orientado_objeto_orm_springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
